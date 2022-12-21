cec3de927179f7806225bbd6f8dc7388fc8b140d
lh-sg <littlehackers.sg@gmail.com>
2022-12-21 16:32:52 +0800

    added files and changes required for production deployment

--- app.js
+++ app.js
@@ -12,7 +12,12 @@ var app = express();
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
-const mongoDB = "mongodb+srv://dk:palmetto19@cluster0.zko2i4h.mongodb.net/?retryWrites=true&w=majority";
+
+// Set up mongoose connection
+const dev_db_url =
+  "mongodb+srv://dk:palmetto19@cluster0.zko2i4h.mongodb.net/?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+
 mongoose.connect(mongoDB,{ useNewUrlParser: true,useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error",console.error.bind(console,"MongoDB connection error:"));
